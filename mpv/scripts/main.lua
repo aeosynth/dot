1a1740e6c1138ca43a84f3ec8fb799aca62aeee0
James Campos <james.r.campos@gmail.com>
2020-07-01 21:10:01 -0700

    nu

---
+++ mpv/scripts/main.lua
@@ -0,0 +1,11 @@
+function trash()
+  os.rename(mp.get_property('path'), '/mnt/.trash/' .. mp.get_property('filename'))
+  mp.command('playlist-remove current')
+end
+mp.register_script_message('trash', trash)
+
+function save()
+  os.rename(mp.get_property('path'), '.save/' .. mp.get_property('filename'))
+  mp.command('playlist-remove current')
+end
+mp.register_script_message('save', save)
